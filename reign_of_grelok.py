8939fb886f7543e3fc6ead852e149f5eb79e97e3
Adrian Rocamora <adrianrocamorav@gmail.com>
2018-11-04 00:32:46 -0600

    Optimization

--- reign_of_grelok.py
+++ reign_of_grelok.py
@@ -6,11 +6,9 @@ gs.routine_010()
 
 key = gs.routine_100()
 
-
-# TODO assign f(key) to a variable and remove the input output
-
-if isinstance(gs.routine_100_map(key), str):
-    print(gs.routine_100_map(key))
+gs_100_mapped = gs.routine_100_map(key)
+if isinstance(gs_100_mapped, str):
+    print(gs_100_mapped)
 else:
-    if gs.routine_100_map(key) == 17:
+    if gs_100_mapped == 17:
         key = gs.routine_101()
